2f00cc6c7a70a35df7d77271810474a0cc7bf90b
nobiasz62 <acsnor055@hengersor.hu>
2025-01-09 10:20:09 +0100

    utolso

--- web/javascript.js
+++ web/javascript.js
@@ -1,22 +1,17 @@
 
-// Az űrlap eseménykezelője
 document.addEventListener('DOMContentLoaded', function () {
-    // Az űrlap elem azonosítása
+ 
     const contactForm = document.getElementById('contactForm');
     const feedbackMessage = document.getElementById('feedbackMessage');
 
-    // Eseménykezelő hozzáadása az űrlaphoz
     contactForm.addEventListener('submit', function (event) {
-        // Alapértelmezett űrlapküldés megakadályozása
+
         event.preventDefault();
 
-        // Üzenet megjelenítése
         feedbackMessage.style.display = 'block';
-
-        // Az űrlap mezőinek törlése
+        
         contactForm.reset();
 
-        // Üzenet eltüntetése 5 másodperc múlva
         setTimeout(function () {
             feedbackMessage.style.display = 'none';
         }, 5000);
